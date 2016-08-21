d413838df82059563178586c3123cd131dade9ed
chandramouli1998 <chandramoulisv1@gmail.com>
2016-08-21 05:58:59 -0500

    Add files via upload

--- Date1.java
+++ Date1.java
@@ -40,19 +40,5 @@ public void displaydate()
 	System.out.printf("%d/%d/%d",getDay(),getMon(),getYear());
 }
 	
-	public static void main(String args[])
-	{
-		Scanner user_input=new Scanner(System.in);
-		Date1 p=new Date1();
-		System.out.println("enter the month");
-		int month=user_input.nextInt();
-		System.out.println("enter the day");
-		int date=user_input.nextInt();
-		System.out.println("enter the year");
-		int year=user_input.nextInt();	
-		p.setDay(date);
-		p.setMon(month);
-		p.setYear(year);
-		p.displaydate();
-	}
+
 }
